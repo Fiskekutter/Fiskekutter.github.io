878793072dad0a21e93d108d5f0caefe77149bec
Søren Hermansen <Soren.harbo.hermansen@gmail.com>
2023-08-23 11:13:48 +0200

    Added logout button

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const userInfo = document.getElementById("user-info");
     const usernameDisplay = document.getElementById("username-display");
     const avatar = document.getElementById("avatar");
+    const logoutButton = document.getElementById("logout-button");
 
     loginButton.addEventListener("click", function() {
         const username = document.getElementById("username").value;
@@ -17,9 +18,31 @@ document.addEventListener("DOMContentLoaded", function() {
             usernameDisplay.textContent = username;
             avatar.style.display = "flex";
             avatar.style.visibility = "visible";
+            avatar.style.padding = "5px";
+            
             userInfo.style.display = "flex";
+            userInfo.style.padding = "3px";
+            
+            logoutButton.style.display = "flex";
+            logoutButton.style.visibility = "visible";
+            logoutButton.style.padding = "5px";
+
+            logoutButton.addEventListener("click", function() {
+                loginForm.style.display = "flex";
+                
+                username.value = "";
+                password.value = "";
+
+                usernameDisplay.textContent = "";
+                userInfo.style.display = "none";
+                avatar.style.display = "none";
+
+                logoutButton.style.display = "none";
+
+            });
         } else {
             alert("Login failed. Please check your username and password.");
         }
     });
+
 });
