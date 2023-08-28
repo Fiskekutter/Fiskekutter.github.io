53b0471deec50876924e1379f5940d66f89772fe
Søren Hermansen <Soren.harbo.hermansen@gmail.com>
2023-08-28 16:14:41 +0200

    Changed login functionality

--- script.js
+++ script.js
@@ -1,45 +1,35 @@
-//document.querySelector('meta[name="viewport"]').setAttribute('content', 'width='+siteWidth+', initial-scale='+scale+'');
-
 document.addEventListener("DOMContentLoaded", function() {
     const loginButton = document.getElementById("login-button");
-    const loginForm = document.getElementById("login-form");
     const userInfo = document.getElementById("user-info");
     const usernameDisplay = document.getElementById("username-display");
     const avatar = document.getElementById("avatar");
     const logoutButton = document.getElementById("logout-button");
+
+    const loginBoxButton = document.getElementById('login-logo');
+    const loginBox = document.getElementById('login-popout');
+
+    var box = false;
+
+    loginBoxButton.addEventListener("click", function(){
+        if(box === false){
+            loginBox.style.display = "block";
+            box = true;
+        } else {
+            loginBox.style.display = "none";
+            box = false;
+        }  
+    });
 
     loginButton.addEventListener("click", function() {
         const username = document.getElementById("username").value;
         const password = document.getElementById("password").value;
 
         if (username === "ad" && password === "ad") {
-            // Hide the login form
-            loginForm.style.display = "none";
 
-            // Display the username
-            usernameDisplay.textContent = username;
-            avatar.style.display = "flex";
-            avatar.style.visibility = "visible";
-            avatar.style.padding = "5px";
+            loginBoxButton.style.display = "none";
+            loginBox.style.display = "none";
             
-            userInfo.style.display = "flex";
-            userInfo.style.padding = "3px";
-            
-            logoutButton.style.display = "flex";
-            logoutButton.style.visibility = "visible";
-            logoutButton.style.padding = "5px";
-
             logoutButton.addEventListener("click", function() {
-                loginForm.style.display = "flex";
-                
-                username.value = "";
-                password.value = "";
-
-                usernameDisplay.textContent = "";
-                userInfo.style.display = "none";
-                avatar.style.display = "none";
-
-                logoutButton.style.display = "none";
 
             });
         } else {
@@ -132,7 +122,7 @@ document.getElementById('fetchBtn').addEventListener('click', async () => {
 
 document.addEventListener("DOMContentLoaded", function() {
     const contactForm = document.getElementById('contactForm');
-    
+
     contactForm.addEventListener('submit', function(event) {
         event.preventDefault();
 
@@ -148,3 +138,4 @@ document.addEventListener("DOMContentLoaded", function() {
 
         })
     });
+
